1afeb95008e79682d3169115a3d9c238f20c120e
mudsahni <muditsahni@msn.com>
2020-12-25 15:11:30 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import 'semantic-ui-css/semantic.min.css'
 
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Home } from './page/Home';
 import { Me } from './page/Me';
 
 function App() {
   return (
-    <Router>
-      <div className="main-container">
-        <main>
-          <Route path='/' component={Home} exact />
-          <Route path='/me' component={Me} exact />
-
-        </main>
-      </div>
-    </Router>
+    <div className="main-container">
+      <Me />
+    </div>
   );
 }
 
